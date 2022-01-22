7b9ac8124d4bd504297023474be3d7d9dd736380
Hashir <mail2hashirpm@gmail.com>
2022-01-22 13:45:01 +0530

    Updates

---
+++ precache-manifest.030863121a642b69730c72ec00df90cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5160e1ce806f2d3d30a7a3504c96e989",
+    "url": "/int/index.html"
+  },
+  {
+    "revision": "83a5468cb40143c4fa91",
+    "url": "/int/static/css/2.5dcc52c6.chunk.css"
+  },
+  {
+    "revision": "de03fd6731907eaf1d5d",
+    "url": "/int/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "83a5468cb40143c4fa91",
+    "url": "/int/static/js/2.ea8eee41.chunk.js"
+  },
+  {
+    "revision": "9143a7c0cbfa518ce39da65db7eaf56a",
+    "url": "/int/static/js/2.ea8eee41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de03fd6731907eaf1d5d",
+    "url": "/int/static/js/main.9b2e487e.chunk.js"
+  },
+  {
+    "revision": "f36bdd0cbf3f3d2f1524",
+    "url": "/int/static/js/runtime-main.adb4dfca.js"
+  },
+  {
+    "revision": "adca41d3777e83416e7ad919fa0da8ac",
+    "url": "/int/static/media/dbank.adca41d3.png"
+  }
+]);
